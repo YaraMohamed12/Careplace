09a59e5ff769227d60763b7a37a3f03c6261f030
adhamsaleh27 <adhamshaaban101@gmail.com>
2024-03-24 02:04:08 +0200

    impoert the user data

--- app/src/main/java/com/example/careplace/NewAccForPat.kt
+++ app/src/main/java/com/example/careplace/NewAccForPat.kt
@@ -1,88 +1,104 @@
 package com.example.careplace
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.careplace.databinding.ActivityNewAccForPatBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 
 class NewAccForPat : AppCompatActivity() {
-    lateinit var mailtxt : TextInputEditText
-    lateinit var passwordtxt : TextInputEditText
-    lateinit var myAuthn1 : FirebaseAuth
-    lateinit var sumbit : Button
-    lateinit var back_btn : ImageView
-    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
+
+    private lateinit var mailtxt: TextInputEditText
+    private lateinit var passwordtxt: TextInputEditText
+    private lateinit var sumbit: Button
+    private lateinit var back_btn: ImageView
+    private lateinit var binding: ActivityNewAccForPatBinding
+    private lateinit var database: FirebaseDatabase
+    private lateinit var myAuthn1: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_acc_for_pat)
+        binding = ActivityNewAccForPatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeViews()
+        register()
+        backbutton()
+    }
 
-        back_btn = findViewById(R.id.imageView2)
+    private fun register() {
+        sumbit.setOnClickListener {
+            val mail = mailtxt.text.toString()
+            val password = passwordtxt.text.toString()
+            if (mail.isNotEmpty() && password.isNotEmpty()) {
+                myAuthn1.createUserWithEmailAndPassword(mail, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                       
+                            val user: FirebaseUser? = myAuthn1.currentUser
+                            user?.let {
+                                val uid = it.uid
+                                val myRef = database.getReference("Users").child(uid) 
+                               
+                                val fullname = binding.editxtname.text.toString()
+                                val phone = binding.editxtphone.text.toString()
+                                val userEmail = mail
+                                val userPassword = password
+                                val BDT = binding.editxtBDT.text.toString()
+                                val ID = binding.editxtID.text.toString()
+                                val gender = binding.editxtGender.text.toString()
+                                val age = binding.editxtAge.text.toString()
+                                val height = binding.editxtHeight.text.toString()
+                                val weight = binding.editxtWeight.text.toString()
+                                val userData = User(fullname, userEmail, phone, ID, height, weight, BDT, age, gender, userPassword)
+
+                                myRef.setValue(userData).addOnSuccessListener {
+                                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                                    SendMailVerification()
+                                }.addOnFailureListener {
+                                    Toast.makeText(this, "Registration Failed: ${it.message}", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        } else {
+                            Toast.makeText(this, "Registration Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, " please Enter your Data", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun initializeViews() {
+        mailtxt = binding.editxtemail
+        passwordtxt = binding.editxtpassword
+        sumbit = binding.sumbitBtn
+        back_btn = binding.imageView2
+        database = FirebaseDatabase.getInstance()
+        myAuthn1 = FirebaseAuth.getInstance()
+    }
+
+    private fun backbutton() {
         back_btn.setOnClickListener {
-          val myIntent3 = Intent(this , LogandSign::class.java)
+            val myIntent3 = Intent(this, LogandSign::class.java)
             startActivity(myIntent3)
         }
-
-
-        Initialzation()
-       Resgister()
-
     }
-    fun Resgister()
-    {
-        sumbit.setOnClickListener {
-            val password = passwordtxt.text.toString()
-            val mail = mailtxt.text.toString()
-            if(mail.isNotEmpty() && password.isNotEmpty()) {
-                myAuthn1.createUserWithEmailAndPassword(mail, password).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        Toast.makeText(this, "Create Successful", Toast.LENGTH_SHORT).show()
-                        SendMailVerifaction()
 
-                    } else {
-                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-                    }
-                }
+    private fun SendMailVerification() {
+        val user = myAuthn1.currentUser
+        user?.sendEmailVerification()?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val myintent = Intent(this, LogInScreen::class.java)
+                startActivity(myintent)
+            } else {
+                Toast.makeText(this, "Verification Mail Is Not Sent: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
             }
-            else
-            {
-                Toast.makeText(this, "You should enter your mail and password", Toast.LENGTH_SHORT).show()
-            }
-
-
-
         }
-
     }
-    fun Initialzation()
-    {
-
-        mailtxt = findViewById(R.id.editxtemail)
-        passwordtxt = findViewById(R.id.editxtpassword)
-        myAuthn1 = FirebaseAuth.getInstance()
-        sumbit = findViewById(R.id.sumbitBtn)
-
-    }
-@SuppressLint("SuspiciousIndentation")
-fun SendMailVerifaction()
-{
- val user = myAuthn1.currentUser
-    user?.sendEmailVerification()?.addOnCompleteListener {
-        if (it.isSuccessful) {
-            val myintent = Intent(this, LogInScreen::class.java)
-            startActivity(myintent)
-        }
-        else{
-            Toast.makeText(this, "Verfiaction Mail Is Not Send", Toast.LENGTH_SHORT).show()
-        }
-
-    }
-
-
-}
-
 }
