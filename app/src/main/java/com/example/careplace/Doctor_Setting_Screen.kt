43f44e8d0e737d701360a48ebff3c6bb10ce7a4d
emannwalid <emyeman1282002@gmail.com>
2024-05-07 22:00:36 +0300

    Medical History

--- app/src/main/java/com/example/careplace/Doctor_Setting_Screen.kt
+++ app/src/main/java/com/example/careplace/Doctor_Setting_Screen.kt
@@ -1,15 +1,62 @@
 package com.example.careplace
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Doctor_Setting_Screen : AppCompatActivity() {
+    lateinit var home_btn : ImageView
+    lateinit var setting_btn : ImageView
+    lateinit var your_profile_btn : ImageView
+    lateinit var calender_btn : ImageView
+    lateinit var chat_btn : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_setting_screen)
+        home_btn = findViewById(R.id.home_icon)
+        setting_btn = findViewById(R.id.setting_icon)
+        your_profile_btn = findViewById(R.id.profile_icon_bar)
+        calender_btn = findViewById(R.id.calender_icon_bar)
+        chat_btn  = findViewById(R.id.goto_chat)
+
+        home_btn.setOnClickListener {
+
+            val myintent1 = Intent(this , Doctor_Home_Screen::class.java)
+            startActivity(myintent1)
+
+        }
+
+        setting_btn.setOnClickListener {
+
+            val myintent2 = Intent(this ,Doctor_Setting_Screen ::class.java)
+            startActivity(myintent2)
+
+        }
+//
+//        your_profile_btn.setOnClickListener {
+//
+//            val myintent3 = Intent(this , ::class.java)
+//            startActivity(myintent3)
+//
+//        }
+//
+        calender_btn.setOnClickListener {
+
+            val myintent4 = Intent(this , Doctor_Calender_Screen::class.java)
+            startActivity(myintent4)
+
+        }
+
+        chat_btn.setOnClickListener {
+
+            val myintent5 = Intent(this ,ContactActivity ::class.java)
+            startActivity(myintent5)
+
+        }
 
     }
 }
--- app/src/main/java/com/example/careplace/My_Profile_Details_for_doc.kt
+++ app/src/main/java/com/example/careplace/My_Profile_Details_for_doc.kt
@@ -3,68 +3,45 @@ package com.example.careplace
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class Patient_Setting_Screen : AppCompatActivity() {
+class My_Profile_Details_for_doc : AppCompatActivity() {
     lateinit var home_btn : ImageView
     lateinit var setting_btn : ImageView
     lateinit var your_profile_btn : ImageView
     lateinit var calender_btn : ImageView
     lateinit var chat_btn : ImageView
-    lateinit var reset_btn : Button
-    lateinit var log_out_btn : Button
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_patient_setting_screen)
-
-        inilaztionlistenr()
-        clicking()
-
-    }
-
-    private fun inilaztionlistenr() {
+        setContentView(R.layout.activity_my_profile_details_for_doc)
         home_btn = findViewById(R.id.home_icon)
         setting_btn = findViewById(R.id.setting_icon)
         your_profile_btn = findViewById(R.id.profile_icon_bar)
         calender_btn = findViewById(R.id.calender_icon_bar)
         chat_btn  = findViewById(R.id.goto_chat)
-        reset_btn = findViewById(R.id.reset_pass_btn)
-        log_out_btn = findViewById(R.id.Log_out)
-    }
 
-    private fun clicking(){
         home_btn.setOnClickListener {
-            val myintent1 = Intent(this , Patient_Home_Screen::class.java)
+            val myintent1 = Intent(this , Doctor_Home_Screen::class.java)
             startActivity(myintent1)
         }
         setting_btn.setOnClickListener {
-            val myintent2 = Intent(this ,Patient_Setting_Screen ::class.java)
+            val myintent2 = Intent(this ,Doctor_Setting_Screen ::class.java)
             startActivity(myintent2)
         }
-        your_profile_btn.setOnClickListener {
-            val myintent3 = Intent(this , My_Profile_Details_for_patient::class.java)
-            startActivity(myintent3)
-        }
+//        your_profile_btn.setOnClickListener {
+//            val myintent3 = Intent(this , ::class.java)
+//            startActivity(myintent3)
+//        }
         calender_btn.setOnClickListener {
-            val myintent4 = Intent(this , Patient_Calender_Screen::class.java)
+            val myintent4 = Intent(this , Doctor_Calender_Screen::class.java)
             startActivity(myintent4)
         }
         chat_btn.setOnClickListener {
-
             val myintent5 = Intent(this ,ContactActivity ::class.java)
             startActivity(myintent5)
+        }
 
-        }
-        reset_btn.setOnClickListener {
-            val intent6 = Intent(this, Reset_Password::class.java)
-            startActivity(intent6)
-        }
-        log_out_btn.setOnClickListener {
-            val intent7 = Intent(this, DocOrPat::class.java)
-            startActivity(intent7)
-        }
     }
 }
